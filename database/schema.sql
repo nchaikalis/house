aa2aa849ae7a32b1068ddeb3535e1d4a9f46884a
nchaikalis <chainicko@gmail.com>
2021-09-25 10:45:46 +0200

    feat: add SQL schema and set the DB info in app properties.

---
+++ database/schema.sql
@@ -0,0 +1,40 @@
+create table person
+(
+    person_id     serial
+        constraint person_pk
+            primary key,
+    first_name    varchar(255) not null,
+    last_name     varchar(255) not null,
+    username      varchar(255) not null,
+    user_password varchar(255) not null
+);
+
+alter table person
+    owner to postgres;
+
+create unique index person_person_id_uindex
+    on person (person_id);
+
+create unique index person_username_uindex
+    on person (username);
+
+create table asset
+(
+    asset_id     serial
+        constraint asset_pk
+            primary key,
+    area         varchar(255) not null,
+    price        integer      not null,
+    availability varchar(255) not null,
+    square_meter integer      not null,
+    person_id    integer
+        constraint person_id
+            references person
+            on update cascade on delete cascade
+);
+
+alter table asset
+    owner to postgres;
+
+create unique index asset_asset_id_uindex
+    on asset (asset_id);
