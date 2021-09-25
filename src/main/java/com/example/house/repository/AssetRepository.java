83596c4e07c950e4db075c3cc24cf0e60096f21a
nchaikalis <chainicko@gmail.com>
2021-09-25 10:47:25 +0200

    feat: add repository (Data Access Object (DAO)) layer.

---
+++ src/main/java/com/example/house/repository/AssetRepository.java
@@ -0,0 +1,9 @@
+package com.example.house.repository;
+
+import com.example.house.entity.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, Integer> {
+}
---
+++ src/main/java/com/example/house/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.house.repository;
+
+import com.example.house.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
