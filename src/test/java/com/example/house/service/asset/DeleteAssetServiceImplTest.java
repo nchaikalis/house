a5a0ac7b73f8ffcf895b6ff69b81ed1a8cd17eb3
nchaikalis <chainicko@gmail.com>
2021-09-26 15:43:09 +0200

    feat: add some tests for the deletion.

--- src/test/java/com/example/house/service/asset/DeleteAssetServiceImplTest.java
+++ src/test/java/com/example/house/service/asset/DeleteAssetServiceImplTest.java
@@ -2,13 +2,16 @@ package com.example.house.service.asset;
 
 import com.example.house.entity.Person;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DeleteAssetServiceImplTest {
-    private final int person_id = 44; // set a correct ID from the DB in order for all tests to work
+    private final int person_id = 44; // set a correct ID from the DB in order for all tests to work.
+    private final int invalidAssetId = -1;
+
     private Person correctPersonId = new Person();
 
     @Autowired
@@ -24,4 +27,24 @@ class DeleteAssetServiceImplTest {
         correctPersonId = null;
     }
 
+    @Test
+    public void DeleteTestNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            assetService.deleteByAssetId(null, invalidAssetId);
+        });
+    }
+
+    @Test
+    public void DeleteTestZero() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            assetService.deleteByAssetId(new Person(), invalidAssetId);
+        });
+    }
+
+    @Test
+    public void DeleteTestWrongAssetId() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            assetService.deleteByAssetId(correctPersonId, invalidAssetId);
+        });
+    }
 }
