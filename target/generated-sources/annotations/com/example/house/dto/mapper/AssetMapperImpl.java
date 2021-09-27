921dd67751539f9ba5ac25c20180517e6f3f061d
nchaikalis <chainicko@gmail.com>
2021-09-27 17:12:43 +0200

    feat: add target folder.

---
+++ target/generated-sources/annotations/com/example/house/dto/mapper/AssetMapperImpl.java
@@ -0,0 +1,45 @@
+package com.example.house.dto.mapper;
+
+import com.example.house.dto.asset.PersonAssetDto;
+import com.example.house.entity.Asset;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-09-27T17:09:40+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
+)
+public class AssetMapperImpl implements AssetMapper {
+
+    @Override
+    public List<PersonAssetDto> getPersonAssetListDto(List<Asset> assetList) {
+        if ( assetList == null ) {
+            return null;
+        }
+
+        List<PersonAssetDto> list = new ArrayList<PersonAssetDto>( assetList.size() );
+        for ( Asset asset : assetList ) {
+            list.add( assetToPersonAssetDto( asset ) );
+        }
+
+        return list;
+    }
+
+    protected PersonAssetDto assetToPersonAssetDto(Asset asset) {
+        if ( asset == null ) {
+            return null;
+        }
+
+        PersonAssetDto personAssetDto = new PersonAssetDto();
+
+        personAssetDto.setAssetId( asset.getAssetId() );
+        personAssetDto.setArea( asset.getArea() );
+        personAssetDto.setPrice( asset.getPrice() );
+        personAssetDto.setAvailability( asset.getAvailability() );
+        personAssetDto.setSquareMeter( asset.getSquareMeter() );
+
+        return personAssetDto;
+    }
+}
---
+++ target/generated-sources/annotations/com/example/house/dto/mapper/PersonMapperImpl.java
@@ -0,0 +1,63 @@
+package com.example.house.dto.mapper;
+
+import com.example.house.dto.person.LogonPersonDto;
+import com.example.house.dto.person.PersonAttemptToLoginDto;
+import com.example.house.dto.person.PersonRegisterDto;
+import com.example.house.entity.Person;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-09-27T17:09:40+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
+)
+public class PersonMapperImpl implements PersonMapper {
+
+    @Override
+    public Person getEntityFromAttemptLoginDto(PersonAttemptToLoginDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Person person = new Person();
+
+        person.setUsername( dto.getUsername() );
+        person.setUserPassword( dto.getUserPassword() );
+
+        return person;
+    }
+
+    @Override
+    public Person getEntityFromPersonRegisterDto(PersonRegisterDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Person person = new Person();
+
+        person.setFirstName( dto.getFirstName() );
+        person.setLastName( dto.getLastName() );
+        person.setUsername( dto.getUsername() );
+        person.setUserPassword( dto.getUserPassword() );
+        person.setRole( dto.getRole() );
+
+        return person;
+    }
+
+    @Override
+    public LogonPersonDto getLogonDto(Person entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        LogonPersonDto logonPersonDto = new LogonPersonDto();
+
+        logonPersonDto.setPersonId( entity.getPersonId() );
+        logonPersonDto.setFirstName( entity.getFirstName() );
+        logonPersonDto.setLastName( entity.getLastName() );
+        logonPersonDto.setUsername( entity.getUsername() );
+        logonPersonDto.setRole( entity.getRole() );
+
+        return logonPersonDto;
+    }
+}
